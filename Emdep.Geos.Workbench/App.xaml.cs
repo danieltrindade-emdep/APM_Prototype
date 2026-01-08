8b08ca4d01ccd1df5070a859e7d135e6f3e3cb23
danieltrindade-emdep <daniel.trindade@emdep.com>
2026-01-08 15:26:21 +0000

    Starting Client App
    Created projects and structures
    Assets defined

---
+++ Emdep.Geos.Workbench/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Emdep.Geos.Workbench
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
